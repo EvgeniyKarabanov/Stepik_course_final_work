afd702a3c61870d306ea4f46a0ca73ce391de21c
EvgeniyKarabanov <karabanych@mail.ru>
2021-11-18 17:22:12 +0900

    добавлены локаторы для корзины - кнопка перехода, заголовок для определения нахождения товаров на странице, и сообщения о том, что корзина пуста

--- pages/locators.py
+++ pages/locators.py
@@ -3,6 +3,9 @@ from selenium.webdriver.common.by import By
 class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link")
+    BASKET_LINK = (By.CSS_SELECTOR, ".basket-mini a")
+    BASKET_TITLE = (By.CSS_SELECTOR, ".basket-title")
+    BASKET_EMPTY_MESSAGE = (By.CSS_SELECTOR, "#content_inner>p")
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
