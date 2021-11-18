d025ad88622307cec9a845d34245c33217e3c720
EvgeniyKarabanov <karabanych@mail.ru>
2021-11-18 13:57:35 +0900

    добавлена страница проверок добавления товаров в корзину проверяется наличие кнопки добавления в корзину, вычислении вспомогательной функции для получения промо-скидки, появление сообщения о добавлении книги в корзину, проверки что действительно книга которую выбрали добавлена в корзину и цена в сообщении о добавлении соответствует цене на странице товара

---
+++ pages/product_page.py
@@ -0,0 +1,29 @@
+import time
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
+from .locators import ProductPageLocators
+import math
+
+
+class ProductPage(BasePage):
+    def should_add_to_cart(self):
+        self.should_be_button_add_to_cart()
+        add_to_basket = self.browser.find_element(*ProductPageLocators.BUTTON_ADD_TO_CART)
+        add_to_basket.click()
+        self.solve_quiz_and_get_code()
+        self.should_be_message_add_to_basket()
+        self.should_be_add_this_book(self.find_element(*ProductPageLocators.NAME_BOOK))
+        self.should_be_add_true_price(self.find_element(*ProductPageLocators.PRICE_BOOK))
+        #time.sleep(500)
+
+    def should_be_button_add_to_cart(self):
+        assert self.is_element_present(*ProductPageLocators.BUTTON_ADD_TO_CART), "Button 'add to basket' is not presented"
+
+    def should_be_message_add_to_basket(self):
+        assert self.is_element_present(*ProductPageLocators.MESSAGE_ADDED_TO_BASKET), "No message about added book"
+
+    def should_be_add_this_book(self, name_book):
+        assert name_book == self.browser.find_elements(*ProductPageLocators.BOOK_ADDED_TO_BASKET)[0].text, "Add wrong book"
+
+    def should_be_add_true_price(self, price_book):
+        assert price_book == self.browser.find_elements(*ProductPageLocators.BOOK_ADDED_TO_BASKET)[2].text, "Add wrong price book"
