dc019e5478b04a6b06c5f5b79cf008739d39e423
EvgeniyKarabanov <karabanych@mail.ru>
2021-11-18 16:01:03 +0900

    добавлены локаторы для BasePage

--- pages/locators.py
+++ pages/locators.py
@@ -1,5 +1,8 @@
 from selenium.webdriver.common.by import By
 
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link")
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
@@ -10,7 +13,6 @@ class LoginPageLocators():
 
 class ProductPageLocators():
     BUTTON_ADD_TO_CART = (By.CSS_SELECTOR, ".btn-add-to-basket")
-    MESSAGE_ADDED_TO_BASKET = (By.CSS_SELECTOR, "#messages")
-    BOOK_ADDED_TO_BASKET = (By.CSS_SELECTOR, ".alertinner strong")
+    MESSAGE_ADDED_TO_BASKET = (By.CSS_SELECTOR, ".alertinner strong")
     NAME_BOOK = (By.CSS_SELECTOR, ".product_main h1")
     PRICE_BOOK = (By.CSS_SELECTOR, ".product_main .price_color")
