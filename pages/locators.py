cfb7a178fb7a66132a75ba011d28b0faba8e4684
EvgeniyKarabanov <karabanych@mail.ru>
2021-11-18 13:54:34 +0900

    Добавлены локаторы для проверки добавления товара в корзину, кнопка, сообщение о добавлении, название книги, цена

--- pages/locators.py
+++ pages/locators.py
@@ -5,5 +5,12 @@ class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
 class LoginPageLocators():
-    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form_id")
-    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form_id")
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators():
+    BUTTON_ADD_TO_CART = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    MESSAGE_ADDED_TO_BASKET = (By.CSS_SELECTOR, "#messages")
+    BOOK_ADDED_TO_BASKET = (By.CSS_SELECTOR, ".alertinner strong")
+    NAME_BOOK = (By.CSS_SELECTOR, ".product_main h1")
+    PRICE_BOOK = (By.CSS_SELECTOR, ".product_main .price_color")
