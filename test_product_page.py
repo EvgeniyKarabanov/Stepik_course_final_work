b1441257124555154a69b8f18dd2729cc6865f64
EvgeniyKarabanov <karabanych@mail.ru>
2021-11-18 17:23:33 +0900

    промаркированы тесты для пропуска предыдущих, добавлен тест перехода со страницы товара в корзину

--- test_product_page.py
+++ test_product_page.py
@@ -1,4 +1,5 @@
 from .pages.product_page import ProductPage
+from .pages.basket_page import BasketPage
 import pytest
 
 '''
@@ -26,6 +27,7 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     page.should_add_to_cart()
     page.should_not_be_success_message_v1()
 
+@pytest.mark.skip
 def test_guest_cant_see_success_message(browser):
     page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
@@ -38,3 +40,9 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.should_add_to_cart()
     page.should_not_be_success_message_v2()
 
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    page = ProductPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()  # открываем страницу
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, link)
+    basket_page.should_be_basket_page()
