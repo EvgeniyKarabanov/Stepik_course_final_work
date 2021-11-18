0877e66d64145abea89f0efaa3e1e3cadc95e1be
EvgeniyKarabanov <karabanych@mail.ru>
2021-11-18 16:06:04 +0900

    сделаны 3 теста для проверки исчезающих сообщений и проверки сообщений которых на странице быть не должно

--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,7 @@
 from .pages.product_page import ProductPage
 import pytest
 
+'''
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
@@ -15,4 +16,25 @@ def test_guest_can_add_product_to_cart(browser, link):
     page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
     page.should_add_to_cart()
+'''
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+@pytest.mark.skip
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()                      # открываем страницу
+    page.should_add_to_cart()
+    page.should_not_be_success_message_v1()
+
+def test_guest_cant_see_success_message(browser):
+    page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()                      # открываем страницу
+    page.should_not_be_success_message_v1()
+
+@pytest.mark.skip
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()  # открываем страницу
+    page.should_add_to_cart()
+    page.should_not_be_success_message_v2()
 
