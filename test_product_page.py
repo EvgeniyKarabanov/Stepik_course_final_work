072d595384c5e7fbaeae7b421ab44fc850bad627
EvgeniyKarabanov <karabanych@mail.ru>
2021-11-20 12:24:45 +0900

    добавлены маркеры need_review перенесены с test_main_page методы test_guest_can_go_to_login_page_from_product_page и test_guest_should_see_login_link_on_product_page т.к. они больше относятся к ProductPage чем к MainPage

--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,6 @@
 from .pages.product_page import ProductPage
 from .pages.basket_page import BasketPage
 from .pages.login_page import LoginPage
-from .pages.base_page import BasePage
 import pytest
 import time
 
@@ -25,23 +24,27 @@ class TestUserAddToBasketFromProductPage(object):
         page.open()  # открываем страницу
         page.should_not_be_success_message_v1()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_cart(self, browser):
         page = ProductPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
         page.open()  # открываем страницу
         page.should_add_to_cart()
 
+@pytest.mark.skip
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
     page.should_add_to_cart()
     page.should_not_be_success_message_v1()
 
+@pytest.mark.skip
 def test_message_disappeared_after_adding_product_to_basket(browser):
     page = ProductPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()  # открываем страницу
     page.should_add_to_cart()
     page.should_not_be_success_message_v2()
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     page = ProductPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()  # открываем страницу
@@ -49,12 +52,27 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     basket_page = BasketPage(browser, link)
     basket_page.should_be_basket_page()
 
-def test_guest_cant_see_success_message(self, browser):
+def test_guest_cant_see_success_message(browser):
     page = ProductPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()  # открываем страницу
     page.should_not_be_success_message_v1()
 
-def test_guest_can_add_product_to_cart(self, browser):
+@pytest.mark.need_review
+def test_guest_can_add_product_to_cart(browser):
     page = ProductPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()  # открываем страницу
     page.should_add_to_cart()
+
+@pytest.mark.need_review
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()                      # открываем страницу
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
