d6959b34f813e4f5db632223211405f65a13cf4d
EvgeniyKarabanov <karabanych@mail.ru>
2021-11-20 10:58:26 +0900

    добавлены локаторы иконки пользователя при авторизациии добавлены локаторы страницы LoginPage для ввода email'аб пароля и подтверждения пароля

--- pages/locators.py
+++ pages/locators.py
@@ -6,6 +6,7 @@ class BasePageLocators():
     BASKET_LINK = (By.CSS_SELECTOR, ".basket-mini a")
     BASKET_TITLE = (By.CSS_SELECTOR, ".basket-title")
     BASKET_EMPTY_MESSAGE = (By.CSS_SELECTOR, "#content_inner>p")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
@@ -13,6 +14,10 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    GUEST_EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
+    GUEST_PASSWORD = (By.CSS_SELECTOR, "#id_registration-password1")
+    GUEST_PASSWORD_CONFIRM = (By.CSS_SELECTOR, "#id_registration-password2")
+    BUTTON_REGISTRATION = (By.NAME, "registration_submit")
 
 class ProductPageLocators():
     BUTTON_ADD_TO_CART = (By.CSS_SELECTOR, ".btn-add-to-basket")
